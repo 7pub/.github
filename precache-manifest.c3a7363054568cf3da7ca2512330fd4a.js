e37eb2cabb0eef7b676fbfd0a2cf0fd1266e02a5
Cali Run <42573065+7pub@users.noreply.github.com>
2022-09-05 18:38:53 +0200

    And I Am...Iron Man

--- precache-manifest.c3a7363054568cf3da7ca2512330fd4a.js
+++ precache-manifest.c3a7363054568cf3da7ca2512330fd4a.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "983cdb0458a6d7e53ef8",
-    "url": "/static/css/main.60c6c1ef.chunk.css"
+    "url": "/static/css/main.chunk.css"
   },
   {
     "revision": "71083722d3554418ca33",
-    "url": "/static/js/2.f63ad4f2.chunk.js"
+    "url": "/static/js/chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.f63ad4f2.chunk.js.LICENSE.txt"
+    "url": "/static/js/chunk.js.LICENSE.txt"
   },
   {
     "revision": "983cdb0458a6d7e53ef8",
-    "url": "/static/js/main.e30d9ebb.chunk.js"
+    "url": "/static/js/main.chunk.js"
   },
   {
     "revision": "2d3f51e3d83e4eb5a3f3",
-    "url": "/static/js/runtime-main.53ef1f00.js"
+    "url": "/static/js/runtime-main.js"
   }
 ]);
--- precache-manifest.js
+++ precache-manifest.js
@@ -5,22 +5,22 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "6f8959d55b4dbd1e5cb1",
-    "url": "/static/css/main.c10e4411.chunk.css"
+    "url": "/static/css/main.chunk.css"
   },
   {
     "revision": "71083722d3554418ca33",
-    "url": "/static/js/2.f63ad4f2.chunk.js"
+    "url": "/static/js/chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/static/js/2.f63ad4f2.chunk.js.LICENSE.txt"
+    "url": "/static/js/chunk.js.LICENSE.txt"
   },
   {
     "revision": "6f8959d55b4dbd1e5cb1",
-    "url": "/static/js/main.754ba035.chunk.js"
+    "url": "/static/js/main.chunk.js"
   },
   {
     "revision": "2d3f51e3d83e4eb5a3f3",
-    "url": "/static/js/runtime-main.53ef1f00.js"
+    "url": "/static/js/runtime-main.js"
   }
 ]);
